a31d6d0cc593b3bac87fd440ada74313c79bd527
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-06-27 17:02:12 +0530

    Add files via upload
    Check for html css and js linking, creating the alert using js and how to write in the website using js file

---
+++ Alert-writing-linking/index.js
@@ -0,0 +1,6 @@
+console.log("HEllo world!");
+console.log("i like pizza");
+window.alert("THis is an alert");
+
+document.getElementById("myH1").textContent= "Hello"
+document.getElementById("myP").textContent = "I like pizza!";
