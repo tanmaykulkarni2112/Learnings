84d4a7a456f3c715f4ecc90af31c2a260b673636
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-07-30 15:31:45 +0530

    Add files via upload

---
+++ pass1/Obj.java
@@ -0,0 +1,10 @@
+public class Obj {
+    String name;
+    int addr;
+    Obj(String nm, int address)
+    {
+        this.name=nm;
+        this.addr=address;
+    }
+}
+    
---
+++ pass1/Pooltable.java
@@ -0,0 +1,7 @@
+public class Pooltable {
+    int first,total_literals;
+    public Pooltable(int f, int l) {
+    this.first=f;
+    this.total_literals=l;
+    }
+}
