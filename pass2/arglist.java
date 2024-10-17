73ad978c476842783122589aaab7b2b88cca65fc
Tanmay Kulkarni <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-10-17 13:16:54 +0530

    Add files via upload

---
+++ pass2/arglist.java
@@ -0,0 +1,13 @@
+package pass2;
+
+public class arglist{
+	 String argname,value;
+	 arglist(String argument){
+	  this.argname=argument;
+	  this.value="";
+	 }
+	 public arglist(String argname, String value) {
+	        this.argname = argname;
+	        this.value = value;
+	    }
+	}
